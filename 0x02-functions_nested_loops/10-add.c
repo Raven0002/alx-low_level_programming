2fd5d45f9e7a5c326864dae9c732cecf313d224b
Raven <marhforimane@gmail.com>
2023-03-21 13:24:49 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ * add - Entry point
+ *
+ * @i1: first number
+ *
+ * @i2: second number
+ *
+ * Return: return results
+ */
+int add(int i1, int i2)
+{
+	return (i1 + i2);
+}
