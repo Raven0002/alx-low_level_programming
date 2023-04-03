c92a3c59a8d8b0ba695def69a59e326529391d39
Raven <marhforimane@gmail.com>
2023-04-03 11:25:55 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ unsigned int_strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *, int size);
+void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#endif/*MAIN.H*/
+#endif
