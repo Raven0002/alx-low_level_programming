ce7209fc012b74ac99f7cbadccec154b0f243881
Raven <marhforimane@gmail.com>
2023-04-05 18:58:40 -0700

    looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,40 @@
 #include "main.h"
-int _sqrt_recursion(int n)
+
 /**
- * _sqrt_recursion - function returns the natural square root of n
+ * _sqrt_recursion - the natural square root of n
  * @n: number whose square root is to be calculated
  *
- * Return: result
+ * Return: square root of n
+ * -1 if not natural square root
  */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 0 || n == 1)
+	return (_sq_root(n, 0));
+}
+
+/**
+ * _sq_root - print sqrt of an integer
+ * @n: the integer
+ * @root: the root of n
+ *
+ * Return: square root of n
+ * -1 if not natural square root
+ */
+
+int _sq_root(int n, int root)
+{
+	if (root * root > n)
 	{
-		return (1);
+		return (-1);
 	}
-	return (sqrt_recursion(n, 0));
+	if (root * root == n)
+	{
+		return (root);
+	}
+	return (_sq_root(n, root + 1));
 }
