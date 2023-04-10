dbc5880d5d6a39fc9815a349038b1f17d2c85029
Raven <marhforimane@gmail.com>
2023-04-10 10:37:28 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main -function prints the number of arguments passed into it
+ * main - function prints the number of arguments passed into it
  * @argc: integer representing the number of command-line arguments
  *
  * @argv: array of strings representing the command-line arguments
  *
- * Return: 0 if success
+ * Return: always 0
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
