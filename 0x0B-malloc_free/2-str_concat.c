01dc2f1db77c2b96b266f036c9086a97dc708af1
Raven <marhforimane@gmail.com>
2023-04-11 17:32:28 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,27 +11,4 @@
  * Return: result
  */
 
-char *str_concat(char *s1, char *s2)
-{
-	char *s;
-	unsigned int n = 0, j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	s = (char *) malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-
-	for (; n < strlen(s1); n++)
-	{
-		s[n] = s1[n];
-	}
-	for (; j < strlen(s2); n++, j++)
-	{
-		s[n] = s2[j];
-	}
-	s[n] = '\0';
-	return (s);
-}
