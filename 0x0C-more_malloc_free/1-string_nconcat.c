9d90a39177f48c5649eaad1ffd60313d0f5f50c2
Raven <marhforimane@gmail.com>
2023-04-14 17:52:03 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
+	char *c;
 	int num, i, j, len;
 
 	num = n;
@@ -36,21 +36,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		num = _strlen(s2);
 	}
 	len = _strlen(s1) + num + 1;
-	p = malloc(sizeof(*p) * len);
-	if (p == NULL)
+	c = malloc(sizeof(*c) * len);
+	if (c == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		p[i] = s1[i];
+		c[i] = s1[i];
 	}
 	for (j = 0; j < num; j++)
 	{
-		p[i + j] = s2[j];
+		c[i + j] = s2[j];
 	}
-	p[i + j] = '\0';
-	return (p);
+	c[i + j] = '\0';
+	return (c);
 }
 
 /**
